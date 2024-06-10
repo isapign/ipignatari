80a4bf437ac3f7470dd85af78d8f1953d0ea9462
ipignatari <isabellypignatari7@gmail.com>
2024-06-09 23:32:30 -0300

    Remove svg de lua e faz ajustes de estilo gerais
    ajusta enum de typist em Introduction.js
    ajusta espaçamento de paddings em aboutMe, experience e introduction styles

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,24 +4,25 @@ import 'react-typist/dist/Typist.css'
 import { Grid } from "@mui/material"
 
 import FadeSection from './FadeSection'
-import logo from '../logo.svg'
 import TranslateText from '../translations/TranslateText'
 import '../styles/Introduction.css'
 
-const Introduction = (props) => {
-  const words = ["jogos", "sistemas", "códigos", "bugs"]
+const Introduction = () => {
+  const words = ["programar", "criar jogos", "resolver bugs", "centralizar div", "cachorros :3", "fazer bolos",  "gravar vídeos" ]
   const [textCounter, setTextCounter] = useState(0)
 
   const handleSetTextCounter = () => {
     setTextCounter(textCounter + 1)
-    if (textCounter >= 3) {
+    if (textCounter >= 6) {
       setTextCounter(0)
     }
   }
 
   const DynamicTypist = () => {
     return <div>
-      <span>eu gosto de criar </span>
+      <span className="introduction-title">
+        eu gosto de 
+      </span>
       <Typist key={textCounter} onTypingDone={handleSetTextCounter}>
         {words[textCounter]}
         <Typist.Delay ms={1500}/>
